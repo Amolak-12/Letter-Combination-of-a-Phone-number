6c8454e776481ce6bf28c0a28d1ae13265b21fda
Amolak-12 <133849186+Amolak-12@users.noreply.github.com>
2024-03-27 15:29:40 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void Solve(string& digit, vector<string> &ans, string& output, int i, vector<string> &Letter, int currentdigit)
+{
+  //Base Case
+  if(i>=digit.size())
+  {
+    ans.push_back(output);
+    return;
+  }
+
+  currentdigit = digit[i] - '0';
+  string value = Letter[currentdigit];
+  for(int j=0;j<value.size();j++)
+    {
+      // Pushing letter
+      output.push_back(value[j]);
+      //Recursive Call
+      Solve(digit, ans, output, i+1, Letter, currentdigit);
+      //Backtracking
+      output.pop_back();
+    }
+}
+int main() {
+
+  string digit = "";
+  cout << "Enter the digits: ";
+  cin >> digits;
+  vector<string>ans;
+  if(digits.length() == 0)
+  {
+    cout <<  "The string is empty as no nos. are given. " << endl;
+    cout << "To print combinations give some nos. " << endl;
+    return 0;
+  }
+  string output = "";
+  int currentdigit = 0;
+  int i = 0;
+
+  vector < string> Letter(10);
+
+  Letter[2] = "abc";
+  Letter[3] = "def";
+  Letter[4] = "ghi";
+  Letter[5] = "jkl";
+  Letter[6] = "mno";
+  Letter[7] = "pqrs";
+  Letter[8] = "tuv";
+  Letter[9] = "wxyz";
+
+  Solve(digits, ans, output, i, Letter, currentdigit);
+
+  cout << "printing answer: " << endl;
+  for(int i=0;i<ans.size();i++){
+    cout << ans[i] << " ";
+  }
+  
+  
+}
